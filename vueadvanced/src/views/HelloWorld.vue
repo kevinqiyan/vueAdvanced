b6b5133b2af74759fc971e6e979cbdcaec492dd5
guokw1 <kevin1025253@gmail.com>
2021-02-04 18:00:25 +0800

    js读取yaml文件-测试

--- vueadvanced/src/views/HelloWorld.vue
+++ vueadvanced/src/views/HelloWorld.vue
@@ -6,6 +6,7 @@
       <thre ref="thre"/>
       <four ref="four"/>
       <five ref="five"/>
+      <!-- <button @click="fs">点击</button> -->
     </div>
   </div>
 </template>
@@ -17,6 +18,8 @@ import thre from '../components/dragModule/thre'
 import four from '../components/dragModule/four'
 import five from '../components/dragModule/five'
 import draggable from 'vuedraggable'
+// import getAll from '../api/index'
+// import build from '../../dist/info.yaml'
 export default {
   name: 'HelloWorld',
   components:{
@@ -37,6 +40,33 @@ export default {
         {'name':five}
       ]
     }
+  },
+  
+  methods: {
+    fs(){
+      let yaml = require('yamljs')
+      console.log(yaml,'++++++',yaml.load);
+      let result = yaml.load('../../dist/info.yaml')
+      console.log(result);
+    },
+    req(){
+      this.$http.get('../api/index.js',(res)=>{
+        console.log(res,'req');
+      })
+    },
+    thrd(){
+      let fs = require('fs')
+      let content = fs.readFileSync('../../dist/info.yaml',{encoding:'utf8'})
+      console.log(content,'thrd内容');
+    }
+  },
+  mounted() {
+    // this.fs()
+    // this.req()
+    // this.thrd()
+    // let abc = new getAll
+    // abc.info()
+    // console.log(localStorage.getItem('info'));
   },
 }
 </script>
